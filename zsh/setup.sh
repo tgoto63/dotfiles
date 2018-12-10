f3e0fd2a1e63f948a6a4300c5a8839bf16a2acf3
tgoto63 <tgoto63@gmail.com>
2018-12-10 19:09:11 +0900

    Add: Zsh settings.

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -1,3 +1,7 @@
+# set installed zsh
+echo /usr/local/bin/zsh | sudo tee -a /etc/shells
+chsh -s /usr/local/bin/zsh
+
 ln -fs ~/Workspace/github.com/tgoto63/dotfiles/zsh/.zshrc ~/.zshrc
 
 source ~/.zshrc
