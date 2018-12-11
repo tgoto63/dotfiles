df569d31f6c9b162ecd6bfd18efb7df8dd83e653
tgoto63 <tgoto63@gmail.com>
2018-12-12 00:26:10 +0900

    Modify(zsh): use oh-my-zsh.

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # tgoto63's dotfiles
 ## How to install 
-
-
+```
+git clone https://github.com/tgoto63/dotfiles.git --recursive
+cd dotfiles
+sh setup.sh
+```
 
 ## How to update Brewfile
 ```
--- zsh/setup.sh
+++ zsh/setup.sh
@@ -2,6 +2,16 @@
 echo /usr/local/bin/zsh | sudo tee -a /etc/shells
 chsh -s /usr/local/bin/zsh
 
+# set up oh-my-zsh
+export ZSH="~/Workspace/github.com/tgoto63/dotfiles/oh-my-zsh";
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# create hard-link to .zshrc
 ln -fs ~/Workspace/github.com/tgoto63/dotfiles/zsh/.zshrc ~/.zshrc
+
+# install power-line-fonts
+git clone https://github.com/powerline/fonts.git --depth=1
+sh ./fonts/install.sh
+rm -rf fonts
 
 source ~/.zshrc
