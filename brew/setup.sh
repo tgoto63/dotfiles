0a19d7b72c4b0c097ce9da92032163e401e98bf3
tgoto63 <tgoto63@gmail.com>
2019-02-08 16:13:55 +0900

    Modify(brew): auto press enter when install home brew.

--- brew/setup.sh
+++ brew/setup.sh
@@ -1,5 +1,5 @@
 # install HomeBrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+yes '' | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # install awesome apps
 brew bundle --file=~/Workspace/github.com/tgoto63/dotfiles/brew/Brewfile
