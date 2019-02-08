7578d7a5eb3378c91a42fbcf652e37e81c69f9d1
tgoto63 <tgoto63@gmail.com>
2019-02-08 19:27:59 +0900

    Modify(brew): agree with Xcode license

--- brew/setup.sh
+++ brew/setup.sh
@@ -3,3 +3,6 @@ yes '' | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebr
 
 # install awesome apps
 brew bundle --file=~/Workspace/github.com/tgoto63/dotfiles/brew/Brewfile
+
+# agree with Xcode license
+sudo xcodebuild -license accept
